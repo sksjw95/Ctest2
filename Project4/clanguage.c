70f5694024ee23497b0be67e95b3f5e16f796490
sksjw95 <tjwldnd95@hanmail.net>
2023-07-01 23:28:52 +0900

    gg

--- Project4/clanguage.c
+++ Project4/clanguage.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 void main() {
+	// 숫자를 논리연산자로 비교할때 숫자가 0이면 false, 0이 아닌 나머지 숫자는 true
 	int data1 = 5, data2 = 3;
 
-	int result1 = data1 > 7;
-	int result2 = data2 <= data1;
-	int result3 = data2 == 7;
-	int result4 = data2 != data1;
+	int result1 = 0 || 1; //   || (or)논리 연산자는 둘중 하나면 참이어도 참이다. --> 1
+	int result2 = 3 && -1; // && (and)논리 연산자는 둘 모두 참이어야 참이다. 하나라도 거짓이 있으면 거짓이다. --> 1
+	int result3 = data1 == 3 || data2 == 3; // --> 1
+	int result4 = data1 == 3 && data2 == 3; // --> 0
+	int result5 = !data1; // -->0
 
-	printf("result : %d, %d, %d, %d\n", result1, result2, result3, result4);
+	printf("result : %d, %d, %d, %d,%d\n", result1, result2, result3, result4, result5);
 }
