ca7822135ca443c2639b683fb985d8ad4368eeb0
sksjw95 <tjwldnd95@hanmail.net>
2023-07-02 16:10:53 +0900

    dd

--- Project4/clanguage.c
+++ Project4/clanguage.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 void main() {
 	// 숫자를 논리연산자로 비교할때 숫자가 0이면 false, 0이 아닌 나머지 숫자는 true
@@ -9,6 +9,6 @@ void main() {
 	int result3 = data1 == 3 || data2 == 3; // --> 1
 	int result4 = data1 == 3 && data2 == 3; // --> 0
 	int result5 = !data1; // -->0
-
+	
 	printf("result : %d, %d, %d, %d,%d\n", result1, result2, result3, result4, result5);
-}
+} */
