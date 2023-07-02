de8ff05aa6120d61c1b08201d3be0a37c198a4a7
sksjw95 <tjwldnd95@hanmail.net>
2023-07-03 01:09:45 +0900

    cc

---
+++ Project4/clanguage6.c
@@ -0,0 +1,16 @@
+/*#include <stdio.h>
+
+void main() {
+	int year = 2016, month = 5, day = 31;
+	day++;
+
+	if (day > 31) {
+		month++;
+		day = 1;
+		if (month > 12) {
+			year++;
+			month = 1;
+		}
+	}
+	printf("Date : %d³â %d¿ù %dÀÏ \n", year, month, day);
+}*/
